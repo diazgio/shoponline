7b749c355f08cc0d494cc64190657750dccb38af
Giordano Martín Díaz Merino <diazgiordano@gmail.com>
2024-02-08 13:01:59 -0500

    Update schema and database url

--- db/migrate/20240202160505_add_category_to_products.rb
+++ db/migrate/20240202160505_add_category_to_products.rb
@@ -1,5 +1,5 @@
 class AddCategoryToProducts < ActiveRecord::Migration[7.1]
   def change
-    add_reference :products, :category, foreign_key: true, type: :uuid
+    add_reference :products, :category, null: false, foreign_key: true, type: :uuid
   end
 end

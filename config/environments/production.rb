92dbef4d5cbf9b85d2b5aba96fb8125aa3839a3c
Giordano Martín Díaz Merino <diazgiordano@gmail.com>
2024-02-09 17:53:34 -0500

    add some styles

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -95,14 +95,14 @@ Rails.application.configure do
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
   # config.public_file_server.enabled = ENV["RAILS_SERVE_STATIC_FILES"].present? || ENV["RENDER"].present?
-  Rails.application.routes.default_url_options[:host] = 'aprendev.online'
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    port: 587,
-    address: ENV["SMTP_ENDPOINT"],
-    user_name: ENV["SMTP_USERNAME"],
-    password: ENV["SMTP_PASSWORD"],
-    authentication: :login,
-    enable_starttls_auto: true
-  }
+  # Rails.application.routes.default_url_options[:host] = 'aprendev.online'
+  # config.action_mailer.delivery_method = :smtp
+  # config.action_mailer.smtp_settings = {
+  #   port: 587,
+  #   address: ENV["SMTP_ENDPOINT"],
+  #   user_name: ENV["SMTP_USERNAME"],
+  #   password: ENV["SMTP_PASSWORD"],
+  #   authentication: :login,
+  #   enable_starttls_auto: true
+  # }
 end
